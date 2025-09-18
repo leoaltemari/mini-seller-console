d1b763f582a8d27ef2916f0f6741dbc72996b255
leo.altemari <leo.altemari@gmail.com>
2025-09-18 18:28:40 -0300

    feat: change failure rate to 10%

--- src/shared/constants/api-simulation.ts
+++ src/shared/constants/api-simulation.ts
@@ -1,4 +1,4 @@
 /** Delay that every API simulation will wait to return a response */
 export const SIMULATED_LATENCY = 1000;
 /** Rate that every API call will fail */
-export const FAILURE_RATE = 0.25;
+export const FAILURE_RATE = 0.1;
