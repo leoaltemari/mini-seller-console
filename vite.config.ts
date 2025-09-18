6947c983433f8fc77b873ae10fd38813d16c4d04
leo.altemari <leo.altemari@gmail.com>
2025-09-18 17:44:28 -0300

    feat: configure github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,14 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   base: './',
+  build: {
+    outDir: 'build',
+  },
   resolve: {
     alias: {
       Components: path.resolve(__dirname, './src/components'),
