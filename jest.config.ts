0f15d01d07f987151b105e7f5431f9248ed75e8c
leo.altemari <leo.altemari@gmail.com>
2025-09-18 16:30:22 -0300

    feat: configure coverage at jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,27 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.jest.json',
+    transform: {
+      tsconfig: [
+        'ts-jest',
+        {
+          tsconfig: '<rootDir>/tsconfig.jest.json',
+          stringifyContentPathRegex: '\\.html$',
+        },
+      ],
     },
   },
-  setupFilesAfterEnv: ['./src/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@context/(.*)$': '<rootDir>/src/context/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@models/(.*)$': '<rootDir>/src/shared/models/$1',
+    '^@constants/(.*)$': '<rootDir>/src/shared/constants/$1',
+    '^@utils/(.*)$': '<rootDir>/src/shared/utils/$1',
+  },
   coverageThreshold: {
     global: {
       branches: 70,
